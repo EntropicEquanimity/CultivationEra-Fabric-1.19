ddc52a4ddc7a223bc97b0969f4f98a902f236a9a
EntropicEquanimity <64940379+EntropicEquanimity@users.noreply.github.com>
2022-07-12 09:07:00 +0800

    Initial commit

---
+++ src/main/java/net/entropicequanimity/cultivationera/CultivationEraMod.java
@@ -0,0 +1,15 @@
+package net.entropicequanimity.cultivationera;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CultivationEraMod implements ModInitializer {
+	public static final String MOD_ID = "cultivationera";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/entropicequanimity/cultivationera/CultivationEraModClient.java
@@ -0,0 +1,11 @@
+package net.entropicequanimity.cultivationera;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CultivationEraModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
